ab485f839ef37948ce65da4e9b7f9fd81b89b3cc
dipta <me.dipta.dhar@gmail.com>
2022-06-02 12:02:50 +0600

    update notfound page

--- src/pages/notfound/notfound.tsx
+++ src/pages/notfound/notfound.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { useHistory, useParams } from 'react-router';
 import styles from "./notfound.module.css";
 
 
 const Notfound = () => {
+  const history = useHistory();
+
+
+  const onClickHandler = () => {
+    history.push("/");
+  }
+
   return (
-    <div>not found</div>
+    <div className={styles.container}>
+      <span className={styles.message}>404!</span>
+      <div className={styles.divHome}>
+        go to &nbsp;
+        <span onClick={onClickHandler}>HOME</span>
+      </div>
+    </div>
   )
 }
 
